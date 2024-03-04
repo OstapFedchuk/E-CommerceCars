60d5e421f706c4df17b0f33405d62e691d670aeb
Ostap-IM <ostap.fedchuk@intramind-srl.com>
2024-03-04 20:16:15 +0100

    Struttura base, Modelli per il DB, Home, About, login e logout

---
+++ store/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+import datetime
+
+
+#Categorie dei prodotti
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+#Clienti
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+#Tutti nostri prodotti
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+    
+    #Nel caso in cui il prodotto è scontato
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.IntegerField(default=1)
+    def __str__(self):
+        return self.name
+
+#Ordini dei Clienti
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product 
---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+'''
+    In questa sezione vanno creati i percorsi 
+    alle nostre pagine web(views.py) all'interno del sito
+'''
+urlpatterns = [
+   path('', views.home, name='home'),
+   path('about/', views.about, name='about'),
+   path('login/', views.login_user, name='login'),
+   path('logout/', views.logout_user, name='logout'),
+]
