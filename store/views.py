241ee1e46adc912368b99dfde935118878d38924
Ostap-IM <ostap.fedchuk@intramind-srl.com>
2024-03-04 21:29:13 +0100

    forms.py(SignUpForm), register.html

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -30,3 +34,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out!"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You hav Registered Successfully!"))
+            return redirect('login')
+        else:
+            messages.success(request, ("There was an error during the Registration, please try again...!"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
